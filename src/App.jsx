430d0ce092ba07a68170d6c1d3346c4199c16a8b
Aakashi <aakashideshwal@gmail.com>
2024-11-22 18:40:30 +0530

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/navbar"
-
+import Footer from "./components/footer.jsx"
 
 function App() {
   return (
 
-<Navbar />
+
+<Footer />
 
  )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css'
+function Footer() {
+    return (
+      <>
+      <footer className="footer">
+        &copy; 2024 NOVELS N ROSES. All rights reserved.
+      </footer>
+      </>  
+    )
+}
+export default Footer;
