18edf2361daf164cdbc306b60b51558ac65b1808
Aakashi <aakashideshwal@gmail.com>
2024-11-23 00:51:15 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/navbar.jsx"
-import Herobanner from "./components/herobanner.jsx"
-import Aboutus from "./components/aboutus.jsx"
-import Sponsors from "./components/sponsors.jsx"
-import Contactus from "./components/contactus.jsx"
-import Footer from "./components/footer.jsx"
+import Navbar from "./components/navbar/navbar.jsx"
+import Herobanner from "./components/herobanner/herobanner.jsx"
+import Aboutus from "./components/about us/aboutus.jsx"
+import Sponsors from "./components/sponsors/sponsors.jsx"
+import Contactus from "./components/contact us/contactus.jsx"
+import Footer from "./components/footer /footer.jsx"
 function App() {
   return (
    <>
